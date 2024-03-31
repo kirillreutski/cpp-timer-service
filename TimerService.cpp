a10ea764ea4eb25c1be1ca9be1379fb3d67abe5f
kirillreutski <k.reutsky@possibilit.nl>
2024-03-31 18:56:27 +0200

    added option to pass yield function into the timer loop

--- TimerService.cpp
+++ TimerService.cpp
@@ -5,44 +5,68 @@ typedef void (*ArrayOfCallbacks) (void);
 
 class TimerService {
     public: 
-    TimerService() {
-        nextTimerToSetup = 0; 
-    }
+        TimerService() {
+            nextTimerToSetup = 0; 
+        }
 
-    int initTimer(int interval, void (*fn)(void)){
-        if (nextTimerToSetup >= 10) return -1; 
-        timerIntervals[nextTimerToSetup] = interval;
-        callbacks[nextTimerToSetup] = fn;
-        int returnValue = nextTimerToSetup; 
-        nextTimerToSetup ++; 
-        return returnValue;
-    }
+        int initTimer(int interval, void (*fn)(void)){
+            if (nextTimerToSetup >= 10) return -1; 
+            timerIntervals[nextTimerToSetup] = interval;
+            callbacks[nextTimerToSetup] = fn;
+            int returnValue = nextTimerToSetup; 
+            nextTimerToSetup ++; 
+            return returnValue;
+        }
 
-    void runTimerImmediately(int timerId) {
-        callbacks[timerId](); 
-    }
+        void runTimerImmediately(int timerId) {
+            callbacks[timerId](); 
+        }
 
-    void changeInterval(int timerId, int interval) {
-        timerIntervals[timerId] = interval;
-    }
+        void changeInterval(int timerId, int interval) {
+            timerIntervals[timerId] = interval;
+        }
 
-    void run(unsigned long currentMillis) {
+        void setYuild(void (*fn)(void)) {
+            yieldFunc = fn; 
+        }
 
-        for (int i = 0; i < nextTimerToSetup; i ++) {
-            if (currentMillis - previousMillis[i] >= timerIntervals[i]) {
-                previousMillis[i] = currentMillis;
-                
-                callbacks[i](); 
+        void setLogger(void (*fn)(const char[])) {
+            logger = fn; 
+            log("Logger initiated");
+        }
+
+        void run(unsigned long currentMillis) {
+            log("Run the cycle");
+            for (int i = 0; i < nextTimerToSetup; i ++) {
+                char str[20]; 
+                sprintf(str, "Running %d timer", i);
+                log(str);
+                if (currentMillis - previousMillis[i] >= timerIntervals[i]) {
+                    previousMillis[i] = currentMillis;
+                    
+                    callbacks[i](); 
+                }
+                log("Yielding...");
+                doYield();
             }
         }
-    }
     
     private: 
         int nextTimerToSetup;
         int timerIntervals[10];
         unsigned long previousMillis[10];
         ArrayOfCallbacks callbacks[10];
+        void (*yieldFunc)(void);
+        void (*logger)(const char[]);
+
+        void doYield(){
+            if (yieldFunc == NULL) return; 
+            yieldFunc(); 
+        }
+
+        void log(const char s[]) {
+            if (logger == NULL) return; 
+            logger(s);
+        }
+
 };
-
-
-
